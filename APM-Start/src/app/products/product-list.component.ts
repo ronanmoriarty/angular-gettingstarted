d409517fa7f8716a3cfcf9fed6c88f0fd692b536
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2019-04-21 12:37:51 +0100

    Add ngOnInit handler to ProductListComponent

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from "./product";
     templateUrl: "./product-list.component.html",
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -34,6 +34,10 @@ export class ProductListComponent{
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
           }
     ];
+
+    ngOnInit(): void {
+        console.log('product-list ngOnInit');
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
